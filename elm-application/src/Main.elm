f20888b2b103b84171e83a3a4f9a3bdc919ceafd
sdia <sdia.pyc@gmail.com>
2021-07-15 22:39:02 +0200

    starting spa

---
+++ elm-application/src/Main.elm
@@ -0,0 +1,110 @@
+module Main exposing (Model, Msg(..), init, main, subscriptions, update, view, viewLink)
+
+-- import Html exposing (..)
+-- import Html.Attributes exposing (..)
+
+import Browser
+import Browser.Navigation as Nav
+import Element exposing (..)
+import Url
+
+
+
+-- MAIN
+
+
+main : Program () Model Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { key : Nav.Key
+    , url : Url.Url
+    }
+
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+    ( Model key url, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( { model | url = url }
+            , Cmd.none
+            )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "URL Interceptor"
+    , body =
+        [ layout [] <|
+            column
+                []
+                [ text "The current URL is: "
+                , el [] (text (Url.toString model.url))
+                , column []
+                    [ viewLink "/home"
+                    , viewLink "/profile"
+                    , viewLink "/reviews/the-century-of-the-self"
+                    , viewLink "/reviews/public-opinion"
+                    , viewLink "/reviews/shah-of-shahs"
+                    ]
+                ]
+        ]
+    }
+
+
+viewLink : String -> Element msg
+viewLink path =
+    row []
+        [ link []
+            { url = path
+            , label = text path
+            }
+        ]
